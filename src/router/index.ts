64cb75dfaa829e4ce2bb83cc6d31112e26bc0ebd
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-04-05 18:27:42 +0200

    Added routes for components

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "@/components/Login.vue";
 import Home from "@/components/Home.vue";
+import Artists from "@/components/Artists.vue";
+import Tracks from "@/components/Tracks.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: "/home",
       name: "Home",
       component: Home,
+    },
+    {
+      path: "/artists",
+      name: "Artists",
+      component: Artists,
+    },
+    {
+      path: "/tracks",
+      name: "Tracks",
+      component: Tracks,
     },
   ],
 });
