c302cfe62de46a902f81b1e47e27862193ebb4fb
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-04-05 18:15:11 +0200

    Tracks initial page

---
+++ src/components/Tracks.vue
@@ -0,0 +1,150 @@
+<template>
+  <div style="display: flex; justify-content: center">
+    <v-tabs v-model="tab">
+      <v-tab value="short_term" @click="changeTimePeriod()">6 weeks</v-tab>
+      <v-tab value="medium_term" @click="changeTimePeriod()">6 months</v-tab>
+      <v-tab value="long_term" @click="changeTimePeriod()">1 year</v-tab>
+    </v-tabs>
+  </div>
+
+  <br />
+
+  <div style="margin: 0 auto">
+    <h1>My Top Tracks</h1>
+    <br />
+    <div>
+      <v-row style="display: flex; justify-content: space-between">
+        <v-col
+          cols="2"
+          v-for="(item, index) in myTopTracks"
+          style="display: flex; justify-content: center"
+        >
+          <v-slide-x-transition>
+            <v-card
+              width="240"
+              rounded
+              elevation="12"
+              :loading="isLoading"
+              :href="item.external_urls.spotify"
+              target="_blank"
+            >
+              <v-img :src="item.album.images[0].url"></v-img>
+              <v-card-title style="display: flex; justify-content: center">
+                {{ item.name }}
+              </v-card-title>
+            </v-card>
+          </v-slide-x-transition>
+        </v-col>
+      </v-row>
+    </div>
+
+    <br />
+
+    <div>
+      <v-data-table-virtual
+        :items="myTopTracks"
+        :headers="trackHeaders"
+        fixed-header
+        :loading="isLoading"
+      >
+      </v-data-table-virtual>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import type Artist from "@/interfaces/Artist";
+import type Track from "@/interfaces/Track";
+import UserTopItems from "@/services/UserTopItems";
+import { tokenStore } from "@/stores/tokenStore";
+
+export default {
+  data() {
+    return {
+      defaultTimeRange: "long_term",
+      myTopArtists: [] as Artist[],
+      myTopTracks: [] as Track[],
+      returnedAccessToken: "",
+      accessTokenStore: tokenStore(),
+      clientId: "e466a474a3de4973ba5fa2b9e4cd9909",
+      artistHeaders: [
+        { title: "Name", value: "name" },
+        { title: "Followers", value: "followers.total" },
+        { title: "Popularity", value: "popularity" },
+      ],
+      trackHeaders: [
+        { title: "Name", value: "name" },
+        { title: "Artists", value: "artists[0].name" },
+        { title: "Album", value: "album.name" },
+        { title: "Popularity", value: "popularity" },
+      ],
+      tab: "long_term",
+      isLoading: true,
+    };
+  },
+  async mounted() {
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code") || "";
+    this.returnedAccessToken = await this.getAccessToken(this.clientId, code);
+
+    this.getTopArtists();
+
+    this.getTopTracks();
+  },
+  methods: {
+    changeTimePeriod() {
+      this.defaultTimeRange = this.tab;
+      this.getTopArtists();
+      this.getTopTracks();
+    },
+
+    getTopArtists() {
+      UserTopItems.fetchTopArtists(
+        this.returnedAccessToken,
+        this.defaultTimeRange
+      )
+        .then((res) => {
+          this.myTopArtists = res.data.items;
+        })
+        .catch((err) => console.log(err));
+
+      this.isLoading = false;
+    },
+
+    getTopTracks() {
+      UserTopItems.fetchTopTracks(
+        this.returnedAccessToken,
+        this.defaultTimeRange
+      )
+        .then((res) => {
+          this.myTopTracks = res.data.items;
+          console.log(this.myTopTracks);
+        })
+        .catch((err) => console.log(err));
+
+      this.isLoading = false;
+    },
+
+    async getAccessToken(clientId: string, code: string): Promise<string> {
+      const verifier = localStorage.getItem("verifier");
+
+      const params = new URLSearchParams();
+      params.append("client_id", clientId);
+      params.append("grant_type", "authorization_code");
+      params.append("code", code);
+      params.append("redirect_uri", "http://localhost:5173/home");
+      params.append("code_verifier", verifier!);
+
+      const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: params,
+      });
+
+      const { access_token } = await result.json();
+
+      return access_token;
+    },
+  },
+};
+</script>
