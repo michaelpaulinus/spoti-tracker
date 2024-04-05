d797f743c59395bf9b5f2577603a8e9a155befba
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-04-05 18:44:48 +0200

    Removed using user's email

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -52,7 +52,7 @@ export default {
         params.append("client_id", clientId);
         params.append("response_type", "code");
         params.append("redirect_uri", "http://localhost:5173/home");
-        params.append("scope", "user-read-email user-top-read");
+        params.append("scope", "user-top-read");
         params.append("code_challenge_method", "S256");
         params.append("code_challenge", challenge);
 
