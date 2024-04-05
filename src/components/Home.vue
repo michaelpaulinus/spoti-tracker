555cde83339cb483deefc7cfc2d184a0402e33aa
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-04-05 18:25:47 +0200

    Updated home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -39,21 +39,44 @@
     </div>
 
     <br />
+    <br />
+    <br />
 
-    <div>
-      <v-data-table-virtual
-        :items="myTopArtists"
-        :headers="artistHeaders"
-        fixed-header
-        :loading="isLoading"
-      >
-      </v-data-table-virtual>
+    <div style="margin: 0 auto">
+      <h1>My Top Tracks</h1>
+      <br />
+      <div>
+        <v-row style="display: flex; justify-content: space-between">
+          <v-col
+            cols="2"
+            v-for="(item, index) in myTopTracks"
+            style="display: flex; justify-content: center"
+          >
+            <v-slide-x-transition>
+              <v-card
+                width="240"
+                rounded
+                elevation="12"
+                :loading="isLoading"
+                :href="item.external_urls.spotify"
+                target="_blank"
+              >
+                <v-img :src="item.album.images[0].url"></v-img>
+                <v-card-title style="display: flex; justify-content: center">
+                  {{ item.name }}
+                </v-card-title>
+              </v-card>
+            </v-slide-x-transition>
+          </v-col>
+        </v-row>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import type Artist from "@/interfaces/Artist";
+import type Track from "@/interfaces/Track";
 import UserTopItems from "@/services/UserTopItems";
 import { tokenStore } from "@/stores/tokenStore";
 
@@ -62,13 +85,19 @@ export default {
     return {
       defaultTimeRange: "long_term",
       myTopArtists: [] as Artist[],
-      myTopTracks: [] as Artist[],
+      myTopTracks: [] as Track[],
       returnedAccessToken: "",
       accessTokenStore: tokenStore(),
       clientId: "e466a474a3de4973ba5fa2b9e4cd9909",
       artistHeaders: [
         { title: "Name", value: "name" },
         { title: "Followers", value: "followers.total" },
+        { title: "Popularity", value: "popularity" },
+      ],
+      trackHeaders: [
+        { title: "Name", value: "name" },
+        { title: "Artists", value: "artists[0].name" },
+        { title: "Album", value: "album.name" },
         { title: "Popularity", value: "popularity" },
       ],
       tab: "long_term",
@@ -81,12 +110,16 @@ export default {
     this.returnedAccessToken = await this.getAccessToken(this.clientId, code);
 
     this.getTopArtists();
+
+    this.getTopTracks();
   },
   methods: {
     changeTimePeriod() {
       this.defaultTimeRange = this.tab;
       this.getTopArtists();
+      this.getTopTracks();
     },
+
     getTopArtists() {
       UserTopItems.fetchTopArtists(
         this.returnedAccessToken,
@@ -94,7 +127,6 @@ export default {
       )
         .then((res) => {
           this.myTopArtists = res.data.items;
-          console.log(this.myTopArtists);
         })
         .catch((err) => console.log(err));
 
@@ -107,9 +139,12 @@ export default {
         this.defaultTimeRange
       )
         .then((res) => {
-          this.myTopTracks = res.data;
+          this.myTopTracks = res.data.items;
+          console.log(this.myTopTracks);
         })
         .catch((err) => console.log(err));
+
+      this.isLoading = false;
     },
 
     async getAccessToken(clientId: string, code: string): Promise<string> {
