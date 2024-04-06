3fde1fabb3050a9b7e6f3ee7a86930c2a401c621
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-04-06 18:45:00 +0200

    Added limit variable

--- src/services/UserTopItems.ts
+++ src/services/UserTopItems.ts
@@ -2,10 +2,12 @@ import type { TopArtistsResponse } from "@/interfaces/TopArtists";
 import type { TopTracksResponse } from "@/interfaces/TopTracks";
 import axios from "axios";
 
+const limit = 10;
+
 class UserTopItems {
   async fetchTopArtists(token: string, time_range: string) {
     return await axios.get<TopArtistsResponse>(
-      `https://api.spotify.com/v1/me/top/artists?time_range=${time_range}&limit=5`,
+      `https://api.spotify.com/v1/me/top/artists?time_range=${time_range}&limit=${limit}`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -16,7 +18,7 @@ class UserTopItems {
 
   async fetchTopTracks(token: string, time_range: string) {
     return await axios.get<TopTracksResponse>(
-      `https://api.spotify.com/v1/me/top/tracks?time_range=${time_range}&limit=5`,
+      `https://api.spotify.com/v1/me/top/tracks?time_range=${time_range}&limit=${limit}`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
