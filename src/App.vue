65b84ec0a11df8708cf9e8f4a2fd1fc650ec5994
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-04-06 14:55:16 +0200

    Added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import Login from "@/components/Login.vue";
-import Home from "@/components/Home.vue";
+import NavBar from "@/components/NavBar.vue";
 </script>
 
 <template>
+  <NavBar></NavBar>
   <RouterView></RouterView>
 </template>
