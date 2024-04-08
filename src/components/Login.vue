5aa3e9966b55f4e883f26db854a3ff9efbc7cb95
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-04-08 18:33:37 +0200

    Changed redirect uri

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -38,8 +38,6 @@ export default {
         store.setToken(accessToken);
         this.accessToken = accessToken;
         const profile = await fetchProfile(accessToken);
-        console.log(accessToken);
-        console.log(profile);
       }
 
       async function redirectToAuthCodeFlow(clientId: string) {
@@ -51,10 +49,7 @@ export default {
         const params = new URLSearchParams();
         params.append("client_id", clientId);
         params.append("response_type", "code");
-        params.append(
-          "redirect_uri",
-          "https://my-spotify-wrapped.netlify.app/home"
-        );
+        params.append("redirect_uri", "https://my-spotify-wrapped.netlify.app");
         params.append("scope", "user-top-read");
         params.append("code_challenge_method", "S256");
         params.append("code_challenge", challenge);
@@ -94,10 +89,7 @@ export default {
         params.append("client_id", clientId);
         params.append("grant_type", "authorization_code");
         params.append("code", code);
-        params.append(
-          "redirect_uri",
-          "https://my-spotify-wrapped.netlify.app/home"
-        );
+        params.append("redirect_uri", "https://my-spotify-wrapped.netlify.app");
         params.append("code_verifier", verifier!);
 
         const result = await fetch("https://accounts.spotify.com/api/token", {
