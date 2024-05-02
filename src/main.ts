a705e6e8d31cca62670adcdae4862a3542edb1ff
Michael Paulinus <michael.paulinus@synthesis.co.za>
2024-05-02 10:51:48 +0200

    Formatting changes

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
 import "./assets/main.css";
-
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
-
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-
 import mitt from "mitt";
-
 import Toast, { type PluginOptions } from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
