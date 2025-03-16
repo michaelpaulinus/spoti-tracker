1e2a1e358e8376481d59137c0e1239ac4089acbc
Michael Paulinus <michaelpaulinus@gmail.com>
2025-03-16 14:44:42 +0200

    Renamed store

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,55 +1,55 @@
-import { defineStore } from "pinia";
-import type User from "@/models/User";
+import { defineStore } from 'pinia';
+import type User from '@/models/User';
 
 type State = {
-  user: User;
+	user: User;
 };
 
-const useUserStore = defineStore("userStore", {
-  persist: true,
-  state: (): State => ({
-    user: {
-      country: "string",
-      display_name: "string",
-      email: "string",
-      explicit_content: {
-        filter_enabled: false,
-        filter_locked: false,
-      },
-      external_urls: {
-        spotify: "string",
-      },
-      followers: {
-        href: "string",
-        total: 0,
-      },
-      href: "string",
-      id: "string",
-      images: [
-        {
-          url: "https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228",
-          height: 300,
-          width: 300,
-        },
-      ],
-      product: "string",
-      type: "string",
-      uri: "string",
-    } as User,
-  }),
-  actions: {
-    setUser(user: User) {
-      this.user = user;
-    },
-    clearUser() {
-      this.user = {} as User;
-    },
-  },
-  getters: {
-    getUser(): User {
-      return this.user;
-    },
-  },
+const useUserStore = defineStore('user', {
+	persist: true,
+	state: (): State => ({
+		user: {
+			country: 'string',
+			display_name: 'string',
+			email: 'string',
+			explicit_content: {
+				filter_enabled: false,
+				filter_locked: false,
+			},
+			external_urls: {
+				spotify: 'string',
+			},
+			followers: {
+				href: 'string',
+				total: 0,
+			},
+			href: 'string',
+			id: 'string',
+			images: [
+				{
+					url: 'https://i.scdn.co/image/ab67616d00001e02ff9ca10b55ce82ae553c8228',
+					height: 300,
+					width: 300,
+				},
+			],
+			product: 'string',
+			type: 'string',
+			uri: 'string',
+		} as User,
+	}),
+	actions: {
+		setUser(user: User) {
+			this.user = user;
+		},
+		clearUser() {
+			this.user = {} as User;
+		},
+	},
+	getters: {
+		getUser(): User {
+			return this.user;
+		},
+	},
 });
 
 export default useUserStore;
