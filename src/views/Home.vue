c41fc40e61b3f6f51cbbc180eb398a86df7447d7
Michael Paulinus <michaelpaulinus@gmail.com>
2025-03-16 14:50:19 +0200

    Removed redundant variables

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,178 +1,168 @@
 <script lang="ts">
-import type Artist from "@/models/Artist";
-import type Track from "@/models/Track";
-import useTokenStore from "@/stores/token";
-import useUserStore from "@/stores/user";
-import type User from "@/models/User";
-import getTopArtists from "@/helpers/getTopArtists";
-import getTopTracks from "@/helpers/getTopTracks";
-import getUserProfile from "@/helpers/getUserProfile";
-import router from "@/router";
-import TrackCard from "@/components/TrackCard.vue";
-import ArtistCard from "@/components/ArtistCard.vue";
+import type Artist from '@/models/Artist';
+import type Track from '@/models/Track';
+import useTokenStore from '@/stores/token';
+import useUserStore from '@/stores/user';
+import type User from '@/models/User';
+import getTopArtists from '@/helpers/getTopArtists';
+import getTopTracks from '@/helpers/getTopTracks';
+import getUserProfile from '@/helpers/getUserProfile';
+import router from '@/router';
+import TrackCard from '@/components/TrackCard.vue';
+import ArtistCard from '@/components/ArtistCard.vue';
 
 export default {
-  components: {
-    ArtistCard,
-    TrackCard,
-  },
+	components: {
+		ArtistCard,
+		TrackCard,
+	},
 
-  setup() {
-    const store = useTokenStore();
-    const uStore = useUserStore();
+	data() {
+		return {
+			defaultTimeRange: 'short_term',
+			myTopArtists: [] as Artist[],
+			myTopTracks: [] as Track[],
+			accessToken: '',
+			accessTokenStore: useTokenStore(),
+			user: {} as User,
+			userStore: useUserStore(),
+			clientId: import.meta.env.VITE_CLIENT_ID,
+			artistHeaders: [
+				{ title: 'Name', value: 'name' },
+				{ title: 'Followers', value: 'followers.total' },
+				{ title: 'Popularity', value: 'popularity' },
+			],
+			trackHeaders: [
+				{ title: 'Name', value: 'name' },
+				{ title: 'Artists', value: 'artists[0].name' },
+				{ title: 'Album', value: 'album.name' },
+				{ title: 'Popularity', value: 'popularity' },
+			],
+			isLoading: true,
+		};
+	},
 
-    return {
-      store,
-      uStore,
-    };
-  },
+	methods: {
+		async changeTimePeriod(time: string) {
+			this.defaultTimeRange = time;
+			this.isLoading = true;
 
-  data() {
-    return {
-      defaultTimeRange: "short_term",
-      myTopArtists: [] as Artist[],
-      myTopTracks: [] as Track[],
-      accessToken: "",
-      accessTokenStore: useTokenStore(),
-      user: {} as User,
-      userTokenStore: useUserStore(),
-      clientId: import.meta.env.VITE_CLIENT_ID,
-      artistHeaders: [
-        { title: "Name", value: "name" },
-        { title: "Followers", value: "followers.total" },
-        { title: "Popularity", value: "popularity" },
-      ],
-      trackHeaders: [
-        { title: "Name", value: "name" },
-        { title: "Artists", value: "artists[0].name" },
-        { title: "Album", value: "album.name" },
-        { title: "Popularity", value: "popularity" },
-      ],
-      isLoading: true,
-    };
-  },
+			this.myTopArtists = await getTopArtists(
+				this.accessToken,
+				this.defaultTimeRange
+			);
 
-  methods: {
-    async changeTimePeriod(time: string) {
-      this.defaultTimeRange = time;
-      this.isLoading = true;
+			this.myTopTracks = await getTopTracks(
+				this.accessToken,
+				this.defaultTimeRange
+			);
 
-      this.myTopArtists = await getTopArtists(
-        this.accessToken,
-        this.defaultTimeRange
-      );
+			this.isLoading = false;
+		},
 
-      this.myTopTracks = await getTopTracks(
-        this.accessToken,
-        this.defaultTimeRange
-      );
+		async getAccessToken(clientId: string, code: string): Promise<string> {
+			const verifier = localStorage.getItem('verifier');
 
-      this.isLoading = false;
-    },
+			const params = new URLSearchParams();
+			params.append('client_id', clientId);
+			params.append('grant_type', 'authorization_code');
+			params.append('code', code);
+			params.append('redirect_uri', import.meta.env.VITE_REDIRECT_URI);
+			params.append('code_verifier', verifier!);
 
-    async getAccessToken(clientId: string, code: string): Promise<string> {
-      const verifier = localStorage.getItem("verifier");
+			const result = await fetch('https://accounts.spotify.com/api/token', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+				body: params,
+			});
 
-      const params = new URLSearchParams();
-      params.append("client_id", clientId);
-      params.append("grant_type", "authorization_code");
-      params.append("code", code);
-      params.append("redirect_uri", import.meta.env.VITE_REDIRECT_URI);
-      params.append("code_verifier", verifier!);
+			const { access_token } = await result.json();
 
-      const result = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: params,
-      });
+			return access_token;
+		},
+	},
 
-      const { access_token } = await result.json();
+	async created() {
+		const params = new URLSearchParams(window.location.search);
+		const code = params.get('code') || '';
 
-      return access_token;
-    },
-  },
+		if (code !== '') {
+			this.accessToken = await this.getAccessToken(this.clientId, code);
+			this.accessTokenStore.setToken(this.accessToken);
+			this.user = await getUserProfile(this.accessToken);
+			this.userStore.setUser(this.user);
+			router.push('/home');
+		} else {
+			this.accessToken = this.accessTokenStore.getToken;
+			this.user = this.userStore.getUser;
+		}
 
-  async created() {
-    const params = new URLSearchParams(window.location.search);
-    const code = params.get("code") || "";
+		this.isLoading = true;
 
-    if (code !== "") {
-      this.accessToken = await this.getAccessToken(this.clientId, code);
-      this.store.setToken(this.accessToken);
-      this.user = await getUserProfile(this.accessToken);
-      this.uStore.setUser(this.user);
-      router.push("/home");
-    } else {
-      this.accessToken = this.accessTokenStore.getToken;
-      this.user = this.userTokenStore.getUser;
-    }
+		this.myTopArtists = await getTopArtists(
+			this.accessToken,
+			this.defaultTimeRange
+		);
 
-    this.isLoading = true;
+		this.myTopTracks = await getTopTracks(
+			this.accessToken,
+			this.defaultTimeRange
+		);
 
-    this.myTopArtists = await getTopArtists(
-      this.accessToken,
-      this.defaultTimeRange
-    );
+		this.isLoading = false;
+	},
 
-    this.myTopTracks = await getTopTracks(
-      this.accessToken,
-      this.defaultTimeRange
-    );
-
-    this.isLoading = false;
-  },
-
-  mounted() {
-    (this as any).$emitter.on("new_time_range", (timeRange: string) =>
-      this.changeTimePeriod(timeRange)
-    );
-  },
+	mounted() {
+		(this as any).$emitter.on('new_time_range', (timeRange: string) =>
+			this.changeTimePeriod(timeRange)
+		);
+	},
 };
 </script>
 
 <template>
-  <div>
-    <h1>My Top Artists</h1>
-    <br />
-    <div class="artist-and-track-container">
-      <ArtistCard
-        v-for="artist in myTopArtists.slice(0, 5)"
-        :artist="artist"
-        :is-loading="isLoading"
-      />
-    </div>
+	<div>
+		<h1>My Top Artists</h1>
+		<br />
+		<div class="artist-and-track-container">
+			<ArtistCard
+				v-for="artist in myTopArtists.slice(0, 5)"
+				:artist="artist"
+				:is-loading="isLoading"
+			/>
+		</div>
 
-    <br />
-    <br />
+		<br />
+		<br />
 
-    <div>
-      <h1>My Top Tracks</h1>
-      <br />
-      <div class="artist-and-track-container">
-        <TrackCard
-          v-for="track in myTopTracks.slice(0, 5)"
-          :track="track"
-          :is-loading="isLoading"
-        />
-      </div>
-    </div>
-  </div>
+		<div>
+			<h1>My Top Tracks</h1>
+			<br />
+			<div class="artist-and-track-container">
+				<TrackCard
+					v-for="track in myTopTracks.slice(0, 5)"
+					:track="track"
+					:is-loading="isLoading"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style scoped>
 .artist-and-track-container {
-  display: flex;
-  flex-direction: row;
-  gap: 1rem;
+	display: flex;
+	flex-direction: row;
+	gap: 1rem;
 }
 
 @media only screen and (max-width: 600px) {
-  .artist-and-track-container {
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-    justify-content: center;
-    align-items: center;
-  }
+	.artist-and-track-container {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		justify-content: center;
+		align-items: center;
+	}
 }
 </style>
